aba5b08f0482b757ba36372c29c613dc7eb1f3cb
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-02-12 11:42:14 +0100

    Enable evaluation of shell scripts in org-mode.

--- my-settings/setup-org-mode.el
+++ my-settings/setup-org-mode.el
@@ -2,4 +2,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/org-mode/contrib/lisp" t)
 (require 'org-install)
 
+;;; Enable evaluation of shell script snippets via org-babel.
+(require 'ob-sh)
+
 (provide 'setup-org-mode)
