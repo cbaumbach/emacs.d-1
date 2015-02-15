6c33a8e9e7411e23dd710d0bef45a7f5a0beb68f
clemens baumbach <clemens.baumbach@foo.bar>
2015-02-15 06:39:01 -0800

    Enable fuzzy matching in ido-mode.

--- my-settings/setup-ido-mode.el
+++ my-settings/setup-ido-mode.el
@@ -4,6 +4,9 @@
 ;;; Stop file system wide autocompletion.
 (setq ido-auto-merge-work-directories-length -1)
 
+;;; Enable flex matching.
+(setq ido-enable-flex-matching 1)
+
 (defun ido-M-x ()
   "Use ido-like selection for execute-extended-command."
   (interactive)
