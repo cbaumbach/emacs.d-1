3831b26959fd74824c0d0bc5d9c8fc01ea7f5eef
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-02-25 09:56:41 +0100

    Bind local latex-mode keys in latex-mode-hook.

--- my-settings/setup-tex-modes.el
+++ my-settings/setup-tex-modes.el
@@ -1,4 +1,6 @@
-(define-key latex-mode-map (kbd "M-}") 'forward-latex-environment)
-(define-key latex-mode-map (kbd "M-{") 'backward-latex-environment)
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (define-key latex-mode-map (kbd "M-}") 'forward-latex-environment)
+            (define-key latex-mode-map (kbd "M-{") 'backward-latex-environment)))
 
 (provide 'setup-tex-modes)
