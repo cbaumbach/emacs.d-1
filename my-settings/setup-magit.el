8d50368f9093e121a3617767849578a39b17faf1
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-03-04 12:43:58 +0100

    Remove some annoying highlighting in magit.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -1,3 +1,8 @@
 (require 'magit)
 
+;;; Remove some annoying highlighting.
+(set-face-attribute 'magit-section-title nil :inherit nil)
+(set-face-attribute 'magit-item-highlight nil :inherit nil)
+(set-face-attribute 'magit-diff-file-header nil :inherit nil)
+
 (provide 'setup-magit)
