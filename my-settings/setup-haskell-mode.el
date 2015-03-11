b0cd71368eed3f3e0204c804f5f2498386c2aa86
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-03-11 10:20:51 +0100

    Only require haskell-mode when under linux.

--- my-settings/setup-haskell-mode.el
+++ my-settings/setup-haskell-mode.el
@@ -1,5 +1,5 @@
-(require 'haskell-mode-autoloads)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(when (string-equal system-type "gnu/linux")
+  (require 'haskell-mode-autoloads)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
 
 (provide 'setup-haskell-mode)
