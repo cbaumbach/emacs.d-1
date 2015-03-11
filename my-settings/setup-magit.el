ef973916de7742d59fc17b2f403733a8b59c665b
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-03-11 10:27:54 +0100

    Only require magit when running under linux.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -1,8 +1,9 @@
-(require 'magit)
+(when (string-equal system-type "gnu/linux")
+  (require 'magit)
 
-;;; Remove some annoying highlighting.
-(set-face-attribute 'magit-section-title nil :inherit nil)
-(set-face-attribute 'magit-item-highlight nil :inherit nil)
-(set-face-attribute 'magit-diff-file-header nil :inherit nil)
+  ;; Remove some annoying highlighting.
+  (set-face-attribute 'magit-section-title nil :inherit nil)
+  (set-face-attribute 'magit-item-highlight nil :inherit nil)
+  (set-face-attribute 'magit-diff-file-header nil :inherit nil))
 
 (provide 'setup-magit)
