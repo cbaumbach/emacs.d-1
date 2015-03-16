1019ef26aa50da2a6893651a19a92025a7952227
clemens baumbach <cb@hal.domain>
2015-03-16 05:01:26 -0700

    Don't require dired-details in emacs >= 24.4.

--- my-settings/setup-dired-mode.el
+++ my-settings/setup-dired-mode.el
@@ -7,11 +7,22 @@
 ;;; List files in human readable format in dired-mode.
 (setq-default dired-listing-switches "-alh")
 
-(require 'dired-details)
-
-;;; Reduce clutter in dired display.
-(setq-default dired-details-hidden-string "--- ")
-(dired-details-install)
+;;; Starting with emacs 24.4, directory details are hidden by default
+;;; via dired-hide-details-mode.  Therefore we only need to load
+;;; dired-details for earlier version of emacs (in which the command
+;;; dired-hide-details-mode is not bound).
+(cond ((not (fboundp 'dired-hide-details-mode))
+       (require 'dired-details)
+       ;; Reduce clutter in dired display.
+       (setq-default dired-details-hidden-string "")
+       (dired-details-install))
+      (t
+       ;; Make ")" also toggle dired-hide-details-mode so we can use
+       ;; the same keys as with dired-details where "(" hides and ")"
+       ;; unhides details.
+       (add-hook 'dired-mode-hook
+                 #'(lambda ()
+                     (local-set-key (kbd ")") 'dired-hide-details-mode)))))
 
 ;;; Adjust M-< und M-> to jump to the first and last file/directory in
 ;;; dired buffer.
