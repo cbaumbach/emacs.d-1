5e1372595600592b7aa7f6bec4f18eef450b2e99
clemens baumbach <cb@hal.domain>
2015-04-05 08:51:27 -0700

    Change haskell indentation mode.

--- my-settings/setup-haskell-mode.el
+++ my-settings/setup-haskell-mode.el
@@ -1,5 +1,8 @@
 (when (string-equal system-type "gnu/linux")
   (require 'haskell-mode-autoloads)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+)
 
 (provide 'setup-haskell-mode)
